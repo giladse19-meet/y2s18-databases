d235d9aea76c5f6e4b1eaa0b807627d34d2f1ee3
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 14:09:29 +0300

    got to part 3/4 delete not working

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,20 +8,33 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(subject_id, student, topic, artical, rating):
+	topic_info = Knowledge(
+		subject_id = subject_id,
+		student = student,
+		topic = topic,
+		artical = artical,
+		rating = rating)
+	session.add(topic_info)
+	session.commit()
+
+#add_article(1, "Gilad", "robotics" , "robotics", 10)
+#add_articale(2, "yair", "basketball", "basketball", 8)
 
 def query_all_articles():
-	pass
+	print(session.query(Knowledge).all())
 
 def query_article_by_topic():
-	pass
+	print(session.query(Knowledge).filter_by(topic=topic))
 
 def delete_article_by_topic():
 	pass
 
 def delete_all_articles():
-	pass
-
+	ready = session.query(Knowledge).all()
+	for i in range(len(ready)):
+		ready[i].delete()
+	session.commit()
+delete_all_articles()
 def edit_article_rating():
 	pass
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -7,17 +7,15 @@ Base = declarative_base()
 
 class Knowledge(Base):
 	__tablename__ = 'knowlenge'
-	id = Colum(Integer, primary_key=True)
+	subject_id = Column(Integer, primary_key=True)
 	student = Column(String)
+	topic = Column(String)
 	artical = Column(String)
 	rating = Column(Integer)
 
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+	def __repr__(self):
+		return ("If you want to learn about {}, you should look at the Wikipedia article called {} We gave this article a rating of {} out of 10!").format(
+				self.topic,
+				self.artical,
+				self.rating)
 
-	pass
